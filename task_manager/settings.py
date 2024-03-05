66dd450c41b6d82ee98d770ffc11fead952f2530
Costard86 <Costard86@gmail.com>
2024-03-05 23:34:40 +0800

    go to review... 2.6 add 'webserver' in ALLOWED_HOST

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.environ.get('DEBUG', False)
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'webserver:9000',
+    'webserver',
 ]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
